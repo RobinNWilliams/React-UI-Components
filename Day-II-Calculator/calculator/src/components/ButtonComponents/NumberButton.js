49f1d15ce301d48c9cc9d414443da5925a8dace4
Rob <rob@users-MacBook-Air.local>
2018-06-12 16:37:36 -0500

    beginnings of calculator in react

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+
+const NumberButton = param => {
+  return (  <div className = "num"> 
+  {param.number}
+    </div> 
+  );
+
+};
+
+export default NumberButton;
+
+
